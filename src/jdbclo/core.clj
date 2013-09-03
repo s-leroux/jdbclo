f58a74bbb78be4d4c59bce569863fa9deaa80b7f
Sylvain Leroux <sylvain@chicoree.fr>
2013-09-03 23:22:46 +0200

    Switched back to dynamic scoping for statements

--- src/jdbclo/core.clj
+++ src/jdbclo/core.clj
@@ -24,13 +24,6 @@
 (defn create-statement [ conn ]
   (.createStatement conn))
 
-(defn prepare-statement 
-  ( [ conn query-string ]
-  (.prepareStatement conn query-string))
-  ( [ query-string ]
-  (.prepareStatement *conn* query-string))
-)
-
 (defn execute-update [ stmt query-string ]
   (.executeUpdate stmt query-string))
 
@@ -42,6 +35,10 @@
 
 (defn close-connection [ conn ]
   (.close conn))
+
+;;
+;; High-level objects are handled using with-/using- functions
+;;
 
 (defmacro with-connection* [ db-spec f & args ]
   `(with-open [conn# (open-connection ~db-spec)]
@@ -64,13 +61,29 @@
 (defmacro using-connection [ conn & body ]
   `(binding[*conn* ~conn] ~@body))
 
+(defn prepare-statement 
+  ( [ conn query-string ]
+  (.prepareStatement conn query-string))
+  ( [ query-string ]
+  (.prepareStatement *conn* query-string))
+)
+
+(defmacro with-prepared-statement [ query-string & body ]
+  `(with-open [stmt# (prepare-statement ~query-string)]
+    (using-prepared-statement stmt# ~@body)
+  )
+)
+
+(defmacro using-prepared-statement [ stmt & body ]
+  `(binding[*stmt* ~stmt] ~@body))
+
 (defn execute! [ query-string ]
   (with-open [stmt (create-statement *conn*)]
     (execute-update stmt query-string)))
 
-(defn execute-using! [ stmt & params ]
-  (doall (map-indexed #(.setObject stmt (inc %1) %2) params))
-  (.executeUpdate stmt)
+(defn execute-using! [ & params ]
+  (doall (map-indexed #(.setObject *stmt* (inc %1) %2) params))
+  (.executeUpdate *stmt*)
 )
 
 (defmacro rollback []
@@ -120,14 +133,26 @@
   )
 )
 
+(defn prepared-statement-demo []
+  (with-connection db-spec
+    (with-prepared-statement "INSERT INTO tbl VALUES(?)"
+      (execute-using! 100)
+      (execute-using! 200)
+      (execute-using! 300)
+    )
+  )
+)
+
 (defn low-level-demo-connect []
   (with-open [conn (open-connection db-spec)
               stmt (prepare-statement conn "INSERT INTO tbl VALUES(?)")]
 
-    (execute-using! stmt 5)
-    (execute-using! stmt 6)
-    (execute-using! stmt 7)
-    (execute-using! stmt 8)
+    (using-prepared-statement stmt
+      (execute-using! 5)
+      (execute-using! 6)
+      (execute-using! 7)
+      (execute-using! 8)
+    )
   )
 )
 
@@ -157,5 +182,6 @@
   []
   (demo-connect)
   (low-level-demo-connect)
+  (prepared-statement-demo)
   (double-connect-demo)
   (println "Hello, World!"))
